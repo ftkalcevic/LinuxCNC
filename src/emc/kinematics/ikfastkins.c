fa1406b864fb6d9e90646ef3fc3e31fa838627d0
Frank Tkalcevic <frank@franksworkshop.com.au>
2013-12-10 10:40:16 +1100

    don't include .c files as it confuses make

--- src/emc/kinematics/ikfastkins.c
+++ src/emc/kinematics/ikfastkins.c
@@ -65,7 +65,7 @@
 
 #define IKFAST_NO_MAIN
 #define IKFAST_HAS_LIBRARY
-#include "ikfast68.Translation3D.0_1_2_f3_4.c"
+#include "ikfast68.Translation3D.0_1_2_f3_4.inc"
 //#include "ikfast68.TranslationZAxisAngle4D.0_1_2_3_f4.c"
 //#include "ikfast68.TranslationDirection5D.0_1_2_3_4.c"
 //#include "ikfast68.Transform6D.0_1_3_4_5_6_f2.c"
